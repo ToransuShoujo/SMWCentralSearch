75d6c1297a5cbf3f6e4ca3fe56307c12d425bbaa
Shoujo <emily@shoujo.tv>
2024-05-21 12:04:05 -0400

    Updated main to include prep window.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import gui
 import dearpygui.dearpygui as dpg
+import database
+import scrape
+import gui
+
 
 gui.create_main_window()
-
+gui.create_prep_window()
+for i in range(0, 6):
+    dpg.render_dearpygui_frame()
+database.insert_smw_hacks(scrape.scrape_hacks())
+dpg.delete_item('win_prep')
 while dpg.is_dearpygui_running():
     dpg.render_dearpygui_frame()
-
 dpg.destroy_context()
+
